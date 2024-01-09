0e6af3d883260d01811787aa7ec4ac1d8af8a821
Gheruha <gheruham@icloud.com>
2024-01-09 11:21:42 +0200

    How to avoid the cancel of the nextLine Scanner.

--- scanner/scanner.java
+++ scanner/scanner.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 class scanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write your name:");
 
+        System.out.println("Write your name:");
         String name = scanner.nextLine();
-        System.out.println("Your name is " + name);
+
+        System.out.println("Write your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Your favorite sport: ");
+        String sport = scanner.nextLine();
+
+        System.out.println("Your name is " + name + "\nYou have " + age + "years old.\nAnd your like " + sport);
         scanner.close();
     }
 }
