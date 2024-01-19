77a66a3529d1d35b91d767f250a0f39547aeda0c
Gheruha <gheruham@icloud.com>
2024-01-19 19:46:00 +0200

    Methods in Java.

---
+++ methods/methods.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class methods {
+    public static void main(String[] args) {
+
+        // int s = sum(5 , 6); Well you can do it this way
+        // System.out.println(s); but the best way now is one that is not in comments.
+
+        System.out.println(sum(5, 6));
+    }
+
+    static int sum(int x, int y) {
+        return x + y;
+    }
+}
